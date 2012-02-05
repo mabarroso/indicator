d9468df6bbb6d84ff2dc80eb5be38ec10c7923d9
mabarroso <mabarroso@mabarroso.com>
2012-02-05 12:11:41 +0100

    Refactoring

---
+++ lib/indicator/spinner.rb
@@ -0,0 +1,68 @@
+# encoding: utf-8
+require File.expand_path('../../indicator/spin', __FILE__)
+
+module Indicator
+  class << self
+
+    def set name = :bar
+      case name
+      when :bar
+        %w{ | / - \\ }
+      when :x
+        %w{ + X }
+      when :<
+        %w{ | < }
+      when :>
+        %w{ | > }
+      when :V
+        %w{ V > ^ }
+      when :dots
+        %w{ : . }
+      when :bars
+        %w{ - = }
+      when :p
+        %w{ p b d q }
+      when :bqpd
+        %w{ bq pd }
+      when :-
+        %w{ - ~ }
+
+      when :arrows
+        %w{ ← ↖ ↑ ↗ → ↘ ↓ ↙ ← ↖ ↑ ↗ → ↘ ↓ ↙  }
+      when :box
+        %w{ ┤ ┘ ┴ └ ├ ┌ ┬ ┐ ┤ ┘ ┴ └ ├ ┌ ┬ ┐ }
+      when :braile
+        %w{ ⣾ ⣽ ⣻ ⢿ ⡿ ⣟ ⣯ ⣷ ⠁ ⠂ ⠄ ⡀ ⢀ ⠠ ⠐ ⠈ }
+
+      else
+        %w{ | / - \\ }
+      end
+    end
+
+    def spinner opts={}
+      opts[:frames] = set opts[:set] unless opts[:frames]
+      Indicator::Spin.new opts
+    end
+
+    def spin opts={}, &blk
+      spinner   = opts[:spinner] || self.spinner(opts)
+      noclean   = opts[:noclean] || false
+      ondemand	= opts[:ondemand] || false
+
+      if ondemand
+        yield spinner
+      else
+        t = Thread.new { yield spinner }
+
+        while t.alive?
+          spinner.spinning
+        end
+
+        t.join
+      end
+
+      spinner.clean unless noclean
+    end
+
+  end
+end
